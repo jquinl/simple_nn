f0e3c282e33a1cd3544adc17cdf97878772ba4ce
Jon Quinlivan <jonquin@gmail.com>
2023-12-01 11:44:02 +0100

    2023-11-30

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <sqlite3.h>
+
+
+#define DEBUG_SHAPE(x) std::cout<< "("<< x.rows()<<","<<x.cols()<<")"<< std::endl
+
 int main(int argc, char *argv[]){
 
-    Eigen::MatrixXd m(2,2);
-    m(0,0)=1;
+    //Input layer 
+    int input_vector_size = 25;//<--- size of a single datapoint
+    int input_layer_size = 10;//<--- Number of training poìnts
 
+    //first layer 
+    int first_layer_size = 10;//<--- Size of the first layer
+
+    Eigen::MatrixXf x = Eigen::MatrixXf::Random(input_layer_size,input_vector_size);
+
+    Eigen::MatrixXf m = Eigen::MatrixXf::Random(first_layer_size,input_vector_size);
+    Eigen::VectorXf b = Eigen::MatrixXf::Random(first_layer_size,1);
+    DEBUG_SHAPE(x);
+    DEBUG_SHAPE(x.transpose());
+    DEBUG_SHAPE(m);
+    DEBUG_SHAPE(b);
+    Eigen::MatrixXf l1 = m * x.transpose();
+    DEBUG_SHAPE(l1);
+
+    std::cout << l1 <<std::endl;
     return 0;
 }
