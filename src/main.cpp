641b77a3a2ba728a43d3d92f3459bc34d6130489
Jon Quinlivan <jonquin@gmail.com>
2023-12-01 15:37:35 +0100

    activation function

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,16 @@
 
 
 #define DEBUG_SHAPE(x) std::cout<< "("<< x.rows()<<","<<x.cols()<<")"<< std::endl
+#define DEBUG_PRINT(x) std::cout << "(" << x << ")" << std::endl
+
+//Activation function struct
+typedef Eigen::MatrixXf (*actfn_ptr)(Eigen::MatrixXf &m1);
+struct Afunc
+{
+    actfn_ptr act_fn;
+    actfn_ptr act_dfn;
+};
+
 
 int main(int argc, char *argv[]){
 
@@ -14,17 +24,23 @@ int main(int argc, char *argv[]){
     //first layer 
     int first_layer_size = 10;//<--- Size of the first layer
 
+    actfn_ptr sigmoid   =  *[]( Eigen::MatrixXf &m) {return Eigen::MatrixXf(1.0f/(1.0f + m.array().exp()));};
+    actfn_ptr sigmoid_d =  *[]( Eigen::MatrixXf &m) {return Eigen::MatrixXf(m.array() * ((1.0f - m.array())));};
+
+    Afunc activ = Afunc{
+                    sigmoid,
+                    sigmoid_d
+                };
+
+
     Eigen::MatrixXf x = Eigen::MatrixXf::Random(input_layer_size,input_vector_size);
 
     Eigen::MatrixXf m = Eigen::MatrixXf::Random(first_layer_size,input_vector_size);
-    Eigen::VectorXf b = Eigen::MatrixXf::Random(first_layer_size,1);
-    DEBUG_SHAPE(x);
-    DEBUG_SHAPE(x.transpose());
-    DEBUG_SHAPE(m);
-    DEBUG_SHAPE(b);
-    Eigen::MatrixXf l1 = m * x.transpose();
-    DEBUG_SHAPE(l1);
+    Eigen::VectorXf b = Eigen::VectorXf::Random(first_layer_size);
 
-    std::cout << l1 <<std::endl;
+    Eigen::MatrixXf l1 = (m * x.transpose()).colwise() + b;
+    DEBUG_PRINT(activ.act_dfn(l1));
+    DEBUG_PRINT(activ.act_fn(l1));
+
     return 0;
 }
